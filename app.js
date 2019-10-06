afd2f393b7d606e5e8da4c5e88f7a63d408ae23d
dumbbass <41546285+developingrxn@users.noreply.github.com>
2019-10-05 23:06:40 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const prefix = '#';
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}! - Prefix '${prefix}'`);
+  client.user.setActivity('TinoRewards', {type: "WATCHING"})
+});
+
+client.on('message', msg => {
+  if (msg.content === prefix + 'ping') {
+    msg.reply('pongxd');
+  }
+});
+
+client.on('message', msg => {
+    if (msg.content === 'bot, please send paypal info') {
+        msg.channel.send('Alright');
+        setTimeout(function(){ 
+            msg.channel.send('Actualmente, no podemos dar el pago, debido a problemas con el sistema de **PayPal**, pero deja aquí tu mail o link de paypal.me/ para que en aproximadamente 3-5 días te podamos enviar el dinero.')
+        }, 1500);
+    }
+  });
+
+client.on('message', (receivedMessage) => {
+    if (receivedMessage.author == client.user) {
+        return
+    }
+    if (receivedMessage.content.startsWith(prefix + 'decir')) {
+        receivedMessage.delete();
+        receivedMessage.channel.send(receivedMessage.content.slice(6));
+    }
+  });
+
+client.login('NTIxMDI5MDExNjk0OTQ0MjY2.XZejwA.2mRVT4qZaeCm8MxykjaFPyCSsdA');
