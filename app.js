98205525facd8743a77c567a3b319e2700db4078
Timo <30553356+y21@users.noreply.github.com>
2019-10-06 12:46:52 +0200

    Optimize code

--- app.js
+++ app.js
@@ -12,25 +12,16 @@ client.on('message', msg => {
   if (msg.content === prefix + 'ping') {
     msg.reply('pongxd');
   }
+  else if (msg.content === 'bot, please send paypal info') {
+    msg.channel.send('Alright');
+    setTimeout(function(){ 
+      msg.channel.send('Actualmente, no podemos dar el pago, debido a problemas con el sistema de **PayPal**, pero deja aquí tu mail o link de paypal.me/ para que en aproximadamente 3-5 días te podamos enviar el dinero.')
+    }, 1500);
+  }
+  else if (msg.content.startsWith(prefix + 'decir') && msg.author.id !== client.user.id) {
+    receivedMessage.delete();
+    receivedMessage.channel.send(receivedMessage.content.slice(6));
+  }
 });
-
-client.on('message', msg => {
-    if (msg.content === 'bot, please send paypal info') {
-        msg.channel.send('Alright');
-        setTimeout(function(){ 
-            msg.channel.send('Actualmente, no podemos dar el pago, debido a problemas con el sistema de **PayPal**, pero deja aquí tu mail o link de paypal.me/ para que en aproximadamente 3-5 días te podamos enviar el dinero.')
-        }, 1500);
-    }
-  });
-
-client.on('message', (receivedMessage) => {
-    if (receivedMessage.author == client.user) {
-        return
-    }
-    if (receivedMessage.content.startsWith(prefix + 'decir')) {
-        receivedMessage.delete();
-        receivedMessage.channel.send(receivedMessage.content.slice(6));
-    }
-  });
 
 client.login('NTIxMDI5MDExNjk0OTQ0MjY2.XZejwA.2mRVT4qZaeCm8MxykjaFPyCSsdA');
